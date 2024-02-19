1c27247bcc27dca268ac30c79a2b96fdddee906e
arbyn-argabioso <arbyn.acosta@gmail.com>
2024-02-19 20:19:21 +0800

    Update main.v2.js

--- public/wedding/js/main.v2.js
+++ public/wedding/js/main.v2.js
@@ -1,8 +1,8 @@
-(function () {
-  if (!isMobile) {
-    const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
-  }
-})();
+// (function () {
+//   if (!isMobile) {
+//     const scroll = new LocomotiveScroll({
+//         el: document.querySelector('[data-scroll-container]'),
+//         smooth: true
+//     });
+//   }
+// })();
